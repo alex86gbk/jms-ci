d5c6a1a66cf9cbec6231b783a46f25ab8c2ff2dd
bian.kai <bian.kai@ruanyun.net>
2019-04-18 15:39:20 +0800

    新增退出功能

---
+++ src/components/Exit.js
@@ -0,0 +1,16 @@
+/**
+ * 退出
+ */
+function exit(req, res) {
+  res.send({
+    result: {
+      status: 1,
+      errMsg: '',
+    }
+  });
+  process.exit();
+}
+
+module.exports = {
+  exit,
+};
--- src/routers/common.js
+++ src/routers/common.js
@@ -4,17 +4,22 @@ const router = express.Router();
 const Project = require('../components/Project');
 const Server = require('../components/Server');
 const Upload = require('../components/Upload');
+const Exit = require('../components/Exit');
 
 router.post('/getProjectList', Project.getProjectList);
 router.post('/saveProject', Project.saveProject);
 router.post('/packageProject', Project.packageProject);
 router.post('/publishProject', Project.publishProject);
 router.post('/deleteProject', Project.deleteProject);
+
 router.post('/getServerList', Server.getServerList);
 router.post('/getServerSelectList', Server.getServerSelectList);
 router.post('/saveServer', Server.saveServer);
 router.post('/deleteServer', Server.deleteServer);
 router.post('/checkServerStatus', Server.checkServerStatus);
+
 router.post('/uploadFile', Upload.uploadFile);
+
+router.post('/exit', Exit.exit);
 
 module.exports = router;
